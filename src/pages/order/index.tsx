175dae5fe0f909cf8387c601b25086da17d591bd
Mengfanliang <2837247375@qq.com>
2021-10-09 10:33:32 +0800

    order页面的基础部分搭建已完成

---
+++ src/pages/order/index.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+
+interface IOrderPageProps {
+}
+
+const OrderPage: React.FunctionComponent<IOrderPageProps> = (props) => {
+	return <div className="OrderPage">
+		Order页面的展示
+	</div>;
+};
+
+export default OrderPage;
